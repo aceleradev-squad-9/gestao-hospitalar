6a9bddf7ed29491025a30d2de680a374368fbb96
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-03-18 14:50:56 -0300

    adicionando dto para classe Hospital

---
+++ src/main/java/gestao/hospital/HospitalDto.java
@@ -0,0 +1,25 @@
+package gestao.hospital;
+
+import javax.validation.constraints.NotBlank;
+
+public class HospitalDto {
+  private String id;
+
+  @NotBlank(message = "O hospital tem que ter nome.")
+  private String name;
+
+  @NotBlank(message = "O hospital tem que ter uma descrição.")
+  private String description;
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+}
