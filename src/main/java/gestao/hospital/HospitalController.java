7a133163c0b8c7be3769c1f80487f153137aebdc
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-03-18 16:09:27 -0300

    add controller para CRUD do Hospital (falta formatar mensagem de validaçao)

---
+++ src/main/java/gestao/hospital/HospitalController.java
@@ -0,0 +1,31 @@
+package gestao.hospital;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hospital")
+public class HospitalController {
+
+  @Autowired
+  HospitalService hospitalService;
+
+  @PostMapping("")
+  public Hospital createHospital(@Valid @RequestBody HospitalDto hospitalDto){
+    return hospitalService.createHospital(hospitalDto);
+  }
+
+}
