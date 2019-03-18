d61b2c6b6c790a0ae5c18d9fe91ba3a0f74996f5
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-03-18 15:31:56 -0300

    add persistencia de hospital no banco de dados

--- src/main/java/gestao/hospital/HospitalService.java
+++ src/main/java/gestao/hospital/HospitalService.java
@@ -1,8 +1,16 @@
 package gestao.hospital;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HospitalService {
 
+  @Autowired
+  HospitalRepository hospitalRepository;
+
   public Hospital createHospital(HospitalDto hospitalDto){
-    return Hospital.createFromDto(hospitalDto);
+    Hospital newHospital = Hospital.createFromDto(hospitalDto);
+    return hospitalRepository.save(newHospital);
   }
 }
