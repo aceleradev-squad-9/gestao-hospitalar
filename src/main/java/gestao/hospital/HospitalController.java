ef995af76c3efd54518f49f425c6582b04120325
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-03-18 16:11:19 -0300

    remover imports nao utilizados e adicionar status correto de resposta

--- src/main/java/gestao/hospital/HospitalController.java
+++ src/main/java/gestao/hospital/HospitalController.java
@@ -1,15 +1,9 @@
 package gestao.hospital;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +18,7 @@ public class HospitalController {
   HospitalService hospitalService;
 
   @PostMapping("")
+  @ResponseStatus(code=HttpStatus.CREATED)
   public Hospital createHospital(@Valid @RequestBody HospitalDto hospitalDto){
     return hospitalService.createHospital(hospitalDto);
   }
