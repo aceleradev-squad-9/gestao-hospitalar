778176a763c8af018ff23e93df76b1a18fd970e3
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-03-18 14:49:14 -0300

    construindo Hospital a partir de HospitalDto

--- src/main/java/gestao/hospital/Hospital.java
+++ src/main/java/gestao/hospital/Hospital.java
@@ -10,6 +10,8 @@ public class Hospital {
 
   private String description;
 
+  private Hospital(){}
+
   public Hospital( String name, String description){
     this.name = name;
     this.description = description;
@@ -25,5 +27,13 @@ public class Hospital {
 
   public String getDescription() {
     return description;
+  }
+
+  public static Hospital createFromDto(HospitalDto dto){
+    Hospital hospital = new Hospital();
+    hospital.id = dto.getId();
+    hospital.name = dto.getName();
+    hospital.description = dto.getDescription();
+    return hospital;
   }
 }
