6ea823372b9f6685df85d17d8d07889d4e937be0
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-03-21 18:27:11 -0300

    create getters and remove id from the Address]

--- src/main/java/gestao/model/address/Address.java
+++ src/main/java/gestao/model/address/Address.java
@@ -1,14 +1,13 @@
 package gestao.model.address;
 
-import org.springframework.data.annotation.Id;
+import javax.validation.constraints.NotBlank;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document
 public class Address {
-  @Id
-  private String id;
-
   private String street;
-
-  private String district;
 
   private String city;
 
@@ -21,4 +20,53 @@ public class Address {
   private Double latitude;
 
   private Double longitude;
+
+  public Address(
+    String street, 
+    String city, 
+    String state, 
+    String cep, 
+    String number, 
+    Double latitude, 
+    Double longitude 
+  ){
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.cep = cep;
+    this.number = number; 
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  @NotBlank(message = "Você deve informar a rua.")
+  public String getStreet() {
+    return street;
+  }
+
+  @NotBlank(message = "Você deve informar a cidade.")
+  public String getCity() {
+    return city;
+  }
+  
+  @NotBlank(message = "Você deve informar o estado.")
+  public String getState() {
+    return state;
+  }
+
+  public String getCep() {
+    return cep;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+
+  public Double getLatitude() {
+    return latitude;
+  }
+
+  public Double getLongitude() {
+    return longitude;
+  }
 }
