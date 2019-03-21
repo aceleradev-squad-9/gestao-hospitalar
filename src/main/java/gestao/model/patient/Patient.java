124429910b55696c2878e626758251e6b8e5e2e4
Caio Queiroz <caiogqueiroz@gmail.com>
2019-03-20 22:26:26 -0300

    completando model patient

--- src/main/java/gestao/model/patient/Patient.java
+++ src/main/java/gestao/model/patient/Patient.java
@@ -1,10 +1,12 @@
 package gestao.model.patient;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import gestao.model.pessoa.Pessoa;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Document(collection="patient")
@@ -12,11 +14,40 @@ public class Patient {
     @Id
     private String id;
 
+    @NotNull(message = "Os dados da pessoa é obrigatório.")
     private Pessoa person;
-    
+
     private LocalDateTime timeCheckIn;
 
     private LocalDateTime timeCheckOut;
 
-    private Patient(){}
+    private ObjectId hospitalId;
+
+    Patient(Pessoa person, LocalDateTime timeCheckIn, LocalDateTime timeCheckOut, ObjectId hospitalId) {
+        this.person = person;
+        this.timeCheckIn = timeCheckIn;
+        this.timeCheckOut = timeCheckOut;
+        this.hospitalId = hospitalId;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public Pessoa getPerson() {
+        return person;
+    }
+
+    public LocalDateTime getTimeCheckIn() {
+        return timeCheckIn;
+    }
+
+    public LocalDateTime getTimeCheckOut() {
+        return timeCheckOut;
+    }
+
+    public ObjectId getHospitalId() {
+        return hospitalId;
+    }
 }
