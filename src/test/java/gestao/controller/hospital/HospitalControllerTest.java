963d4f4f5cfdd6990d693c038b01446443f08dd8
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-03-26 13:11:04 -0300

    Verificar se o controlador esta enviando o objeto correto para o serviço

--- src/test/java/gestao/controller/hospital/HospitalControllerTest.java
+++ src/test/java/gestao/controller/hospital/HospitalControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -28,8 +29,12 @@ public class HospitalControllerTest {
   @Autowired
   private MockMvc mvc;
 
-  @MockBean
+  @MockBean(name="hospitalService")
   private HospitalService mockedHospitalService;
+
+  @InjectMocks
+  @Autowired
+  private HospitalController hospitalController;
 
   @Test
   @DisplayName("Deve receber o body em formato json com as info do hospital criado e http status code 201.")
@@ -74,5 +79,32 @@ public class HospitalControllerTest {
     );
   }
 
+  @Test
+  @DisplayName("Verificar se o controlador está utilizando a instancia correta de HospitalDto para o método HospitalService.createHospital")
+  public void shouldSendTheCorrectInstanceOfHospitalDtoToCreateHospitalMethod(){
+    final HospitalDto hospitalDto = new HospitalDto(
+      "Hospital 1", 
+      "Descrição do hospital 1", 
+      123, 
+      new Address(
+        1L,
+        "Rua do Hospital 1",
+        "Cidade do hospital 1",
+        "Bairro do hospital 1",
+        "Estado do hospital 1",
+        "12345678",
+        "23D",
+        34.234,
+        12.121
+      )  
+    );
 
+    when(
+      mockedHospitalService.createHospital(hospitalDto)
+    ).thenReturn(Hospital.createFromDto(hospitalDto));
+
+    hospitalController.createHospital(hospitalDto);
+
+    verify(mockedHospitalService, times(1)).createHospital(hospitalDto);
+  }
 }
