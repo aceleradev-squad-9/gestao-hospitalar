e118c11f7e099d2ddf3f7d163cb4b66e1715f3ff
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-03-30 08:44:23 -0300

    Adicionar testes unitarios para a remoçao de serviços com HospitalService^

--- src/test/java/gestao/service/hospital/HospitalServiceTest.java
+++ src/test/java/gestao/service/hospital/HospitalServiceTest.java
@@ -222,7 +222,7 @@ public class HospitalServiceTest {
   }
 
   @Test
-  @DisplayName("Deve retornar uma lista com os hospitais recebidos de HospitalRepository")
+  @DisplayName("Deve retornar uma lista com os hospitais recebidos de HospitalRepository.")
   public void shouldReturnAListWithAllHospitalsUsingHospitalRepository(){
     final HospitalDto hospitalDto1 = new HospitalDto(
       "Hospital 1", 
@@ -268,7 +268,7 @@ public class HospitalServiceTest {
     final List<Hospital> allHospitalsFromHospitalService = hospitalService.findAll();
 
     verify(mockedHospitalRepository, times(1)).findAll();
-    
+
     assertEquals(allHospitalsFromHospitalService, allHospitals);
     assertEquals(allHospitalsFromHospitalService.size(), allHospitals.size());
     
@@ -278,5 +278,53 @@ public class HospitalServiceTest {
         allHospitals.get(i)
       );
     }
+  }
+
+  @Test
+  @DisplayName("Deve remover o hospital correto encontrado por HospitalService.findById.")
+  public void shouldDeleteTheCorrectHospital(){
+    final HospitalDto hospitalDto = new HospitalDto(
+      "Hospital 1", 
+      "Descrição do hospital 1", 
+      123, 
+      new Address(
+        1L,
+        "Rua do Hospital 1",
+        "Cidade do hospital 1",
+        "Bairro do hospital 1",
+        "Estado do hospital 1",
+        "12345678",
+        "23D",
+        34.234,
+        12.121
+      )  
+    );
+
+    final Long EXISTING_ID = 1L;
+
+    final Hospital hospitalTobeDeleted = Hospital.createFromDto(hospitalDto);
+    when(mockedHospitalRepository.findById(EXISTING_ID)).thenReturn(
+      Optional.of(hospitalTobeDeleted)
+    );
+
+    hospitalService.delete(EXISTING_ID);
+
+    verify(mockedHospitalRepository, times(1)).findById(EXISTING_ID);
+
+    verify(mockedHospitalRepository, times(1)).delete(hospitalTobeDeleted);
+  }
+
+  @Test
+  @DisplayName("Deve jogar HospitalNotFoundException quando nenhum hospital possui o id enviado para HospitalService.delete")
+  public void shouldThrowHospitalNotFoundException(){
+    final Long NOT_EXISTING_ID = 1L;
+    when(mockedHospitalRepository.findById(NOT_EXISTING_ID)).thenThrow(
+      new HospitalNotFoundException()
+    );
+
+    assertThrows(
+      HospitalNotFoundException.class, 
+      () -> hospitalService.delete(NOT_EXISTING_ID)
+    );
   }
 }
