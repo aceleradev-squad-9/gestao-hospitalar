93668af9786595f9e5a046ade8eb22f7072b754b
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-04-07 16:38:37 -0300

    remove hash code not being used

--- src/main/java/gestao/model/product/Product.java
+++ src/main/java/gestao/model/product/Product.java
@@ -94,14 +94,6 @@ public class Product extends BaseEntity {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
