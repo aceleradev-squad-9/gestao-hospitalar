fa9551717532d21b603aa1b60c8a515dac59fecf
Edmilson Santana <edmilsonmgsantana@gmail.com>
2019-04-08 16:22:34 -0300

    Disponibilizando testes para o HospitalPatientController.

--- src/main/java/gestao/exception/patient/PatientAlreadyHasCheckInOnHospitalException.java
+++ src/main/java/gestao/exception/patient/PatientAlreadyHasCheckInOnHospitalException.java
@@ -3,7 +3,7 @@ package gestao.exception.patient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.CONFLICT)
 public class PatientAlreadyHasCheckInOnHospitalException extends RuntimeException {
 
 	/**
