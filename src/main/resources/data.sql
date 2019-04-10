f018b4e9f815d47a6ab5a9e41bfb874519de7ccd
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2019-04-09 23:27:22 -0300

    inserts para banco de sangue

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO PRODUCT (name, description) VALUES('Banco de Sangue A+', 'Produto referente a um banco de sangue')
-INSERT INTO BLOOD_BANK ()
+INSERT INTO PRODUCT (name, description, deleted) VALUES('Banco de Sangue AB+', 'Produto referente ao banco de sangue para o tipo AB+.', false);
+INSERT INTO PRODUCT (name, description, deleted) VALUES('Banco de Sangue AB-', 'Produto referente ao banco de sangue para o tipo AB-.', false);
+INSERT INTO PRODUCT (name, description, deleted) VALUES('Banco de Sangue O+', 'Produto referente ao banco de sangue para o tipo O+.', false);
+INSERT INTO PRODUCT (name, description, deleted) VALUES('Banco de Sangue O-', 'Produto referente ao banco de sangue para o tipo O-.', false);
